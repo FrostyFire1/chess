6737782ee35b3f924b9dcddcc98598d47ae42234
FrostyFire1 <KamilBaron02@gmail.com>
2022-06-23 13:57:11 +0200

    Add valid_moves for King

--- lib/chess_pieces/king.rb
+++ lib/chess_pieces/king.rb
@@ -16,4 +16,22 @@ class King
       "♔"
     end
   end
+
+  def valid_moves(board)
+    steps = [[1,-1],[1,1],[-1,-1],[-1,1],
+             [1,0],[-1,0],[0,1],[0,-1]]
+    row, column = @position
+    valid_moves = []
+    steps.each do |step|
+      row_step, column_step = step
+      new_row, new_column = [row+row_step, column+column_step]
+      next unless new_row.between?(0,7) and new_column.between?(0,7) # Check chess board bounds
+
+      square_content = board[new_row][new_column]
+      next if square_content.color == @color unless square_content.nil? # Check if square has a chess piece of the same color
+      
+      valid_moves << [new_row,new_column] 
+    end
+    valid_moves
+  end
 end
--- spec/chess_piece/king_spec.rb
+++ spec/chess_piece/king_spec.rb
@@ -1,6 +1,6 @@
 require "chess_pieces/king.rb"
 require_relative "piece_behavior.rb"
-
+require "chess_pieces/pawn.rb"
 
 
 
@@ -37,4 +37,67 @@ describe King do
       end
     end
   end 
+
+  describe "#valid_moves" do
+    context "Returns valid moves when the king is" do
+      let(:board) { Array.new(8) { Array.new(8) }}
+
+      it "in the top right corner" do
+      top_right_king = described_class.new(0,7,"white")
+      valid_moves = top_right_king.valid_moves(board)
+      expected  = [[0,6],[1,6],[1,7]]
+      expect(valid_moves).to match_array(expected)
+      end
+
+      it "in the top left corner" do
+        top_left_king = described_class.new(0,0,"white")
+        valid_moves = top_left_king.valid_moves(board)
+        expected  = [[0,1],[1,1],[1,0]]
+        expect(valid_moves).to match_array(expected)
+      end
+
+      it "in the bottom left corner" do
+        bottom_left_king = described_class.new(7,0,"white")
+        valid_moves = bottom_left_king.valid_moves(board)
+        expected  = [[6,0],[6,1],[7,1]]
+        expect(valid_moves).to match_array(expected)
+      end
+
+      it "in the bottom right corner" do
+        bottom_right_king = described_class.new(7,7,"white")
+        valid_moves = bottom_right_king.valid_moves(board)
+        expected  = [[7,6],[6,6],[6,7]]
+        expect(valid_moves).to match_array(expected)
+      end
+
+      it "on the edge of the board" do
+        edge_king = described_class.new(3,7,"white")
+        valid_moves = edge_king.valid_moves(board)
+        expected  = [[4,7],[4,6],[3,6],[2,6],[2,7]]
+        expect(valid_moves).to match_array(expected)
+      end
+    end
+
+    context "Considers other pieces in the way" do
+      let(:board) { Array.new(8) { Array.new(8) }}
+
+      it "stops before an ally piece" do
+        king = described_class.new(4,5,"white")
+        allied_pawn = Pawn.new(4,6,"white")
+        board[4][6] = allied_pawn
+        valid_moves = king.valid_moves(board)
+        expected  = [[3,5],[3,4],[4,4],[5,4],[5,5],[5,6],[3,6]]
+        expect(valid_moves).to match_array(expected)
+      end
+
+      it "allows attack of enemy piece" do
+        king = described_class.new(7,3,"white")
+        enemy_pawn = Pawn.new(6,3,"black")
+        board[6][3] = enemy_pawn
+        valid_moves = king.valid_moves(board)
+        expected  = [[7,2],[6,2],[6,3],[6,4],[7,4]]
+        expect(valid_moves).to match_array(expected)
+      end
+    end
+  end
 end
